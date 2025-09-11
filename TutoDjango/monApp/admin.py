2574a2ae30c1c09e5922a3d81ddca86c380346af
SNIVEAU <odevva@gmail.com>
2025-09-11 10:18:44 +0200

    Fin du td2

--- TutoDjango/monApp/admin.py
+++ TutoDjango/monApp/admin.py
@@ -10,11 +10,50 @@ class ProduitInline(admin.TabularInline):
 class CategorieAdmin(admin.ModelAdmin):
     model = Categorie
     inlines = [ProduitInline]
+
+
+
+class ProduitFilter(admin.SimpleListFilter):
+    title = 'filtre produit'
+    parameter_name = 'custom_status'
+
+    def lookups(self, _, __):
+        return (
+            ('OnLine', 'En ligne'),
+            ('OffLine', 'Hors ligne'),
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() == 'OnLine':
+            return queryset.filter(statut=1)
+        if self.value() == 'OffLine':
+            return queryset.filter(statut=0)
+        return queryset
+
+def set_Produit_online(modeladmin, request, queryset):
+ queryset.update(statut=1)
+set_Produit_online.short_description = "Mettre en ligne"
+def set_Produit_offline(modeladmin, request, queryset):
+ queryset.update(statut=0)
+set_Produit_offline.short_description = "Mettre hors ligne"
+
+
 class ProduitAdmin(admin.ModelAdmin):
     model = Produit
-    list_display = ["refProd", "intituleProd", "prixUnitaireProd", "dateFabProd", "categorie", "status"]
-    list_editable = ["intituleProd", "prixUnitaireProd", "dateFabProd"]
-    radio_fields = {"status": admin.VERTICAL}
+    list_display = ["refProd", "intituleProd", "prixUnitaireProd", "dateFabrication", "categorie", "statut"]
+    list_editable = ["intituleProd", "prixUnitaireProd", "dateFabrication"]
+    radio_fields = {"statut": admin.VERTICAL}
+    search_fields = ('intituleProd', 'dateFabrication')
+    list_filter = (ProduitFilter,)
+    date_hierarchy = 'dateFabrication'
+    ordering = ('-dateFabrication',)
+    actions = [set_Produit_online, set_Produit_offline]
+
+from decimal import Decimal, ROUND_HALF_UP
+
+def prixTTCProd(instance):
+    return (instance.prixUnitaireProd * Decimal('1.20')).quantize(Decimal('0.01'), rounding=ROUND_HALF_UP)
+prixTTCProd.short_description = "Prix TTC"
 
 admin.site.register(Produit, ProduitAdmin)  
 admin.site.register(Categorie)
